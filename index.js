bebe03148ce3d4537cf7be929977e4c4810cdd4e
Neyasbltb <neyasbltb_88@mail.ru>
2020-10-20 00:50:00 +0300

    Попытка запуска с ssl [2]

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let fs = require('fs');
+let http = require('http');
+let https = require('https');
 let express = require('express');
 let app = express();
 let expressWs = require('express-ws')(app);
@@ -6,12 +9,27 @@ let cors = require('cors');
 
 const PORT = process.env.PORT || 8100;
 
+let server;
+
+if (process.env.SSL) {
+    const key = fs.readFileSync(process.env.KEY, 'utf8');
+    const cert = fs.readFileSync(process.env.CERT, 'utf8');
+    const credentials = { key, cert };
+
+    server = https.createServer(credentials, app);
+} else {
+    server = http.createServer(app);
+}
+
 app.use(cors());
 
 app.get('/', (req, res, next) => res.send('ok'));
 
 app.ws('/', (ws, req) => {
-    ws.on('message', (msg) => wss.clients.forEach((client) => client.send(msg)));
+    ws.on('message', (msg) => {
+        console.log('clients: ', wss.clients);
+        wss.clients.forEach((client) => client.send(msg));
+    });
 });
 
-app.listen(PORT, () => console.log(`Сервер запущен на порту: ${PORT}`));
+server.listen(PORT, () => console.log(`Сервер запущен на порту: ${PORT}`));
